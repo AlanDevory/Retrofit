27c6425750ec82c1399c53fef267324e4c313a65
AlanDevory <alan.gortega@alumnos.udg.mx>
2022-05-27 03:25:07 -0500

    Initial commit

---
+++ app/src/main/java/gomez/alan/retrofit/APIService.kt
@@ -0,0 +1,12 @@
+package gomez.alan.retrofit
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface APIService {
+        @GET
+       suspend fun getDogsByBreeds(@Url url:String):Response<DogResponse>
+
+
+}
---
+++ app/src/main/java/gomez/alan/retrofit/DogViewHolder.kt
@@ -0,0 +1,16 @@
+package gomez.alan.retrofit
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import gomez.alan.retrofit.databinding.ItemDogBinding
+
+class DogViewHolder(view: View):RecyclerView.ViewHolder(view) {
+    private val binding = ItemDogBinding.bind(view)
+    fun bind(image:String){
+        Picasso.get()
+            .load(image)
+            .into(binding.ivDog)
+        
+    }
+}
